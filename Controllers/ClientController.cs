142fbb6ed6eb6efca1d60549a0781a30d52d00b3
ANAS ANASRI <100525138+ANASRIANAS@users.noreply.github.com>
2022-12-02 17:21:06 +0100

    Add project files.

---
+++ Controllers/ClientController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GestionClient.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GestionClient.Controllers
+{
+    public class ClientController : Controller
+    {
+        public IActionResult Index()
+        {
+            var client = new Client()
+            {
+                Id = 1,
+                Nom = "anasri",
+                Prenom = "anas",
+                Ville = "Casa"
+            };
+
+            return View(client);
+        }
+
+        public IActionResult pro()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Models/Client.cs
@@ -0,0 +1,10 @@
+﻿namespace GestionClient.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public string Ville { get; set; }
+    }
+}
